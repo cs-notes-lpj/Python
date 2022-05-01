560722d61189828d131149ede25e289adab9ca20
lpj-s3 <374453156@qq.com>
2022-05-01 08:37:02 +0800

    update

--- js/copyright.js
+++ js/copyright.js
@@ -2,7 +2,7 @@ let copyright = (hook) => {
   let tmp = `
     <footer style="border-top: 1px solid rgba(0,0,0,.07); padding-top: 1rem;">
       <span>
-        &copy; 2022, <a href="https://liupj.top/about/">lpj</a>
+        &copy; 2022, <a href="https://liupj.top/homepage">lpj</a>
       </span>
     </footer>
   `;
